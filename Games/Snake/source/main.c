af9337602fec7cf996bf02d43a4f6afe0237d91f
Nucax <louisthevis84@gmail.com>
2025-10-17 16:22:46 +0200

    Refactor Snake game to use Citro2D graphics
    Updated the Snake game to use Citro2D for rendering and adjusted grid dimensions.

--- Games/Snake/source/main.c
+++ Games/Snake/source/main.c
@@ -1,130 +1,143 @@
 #include <3ds.h>
-#include <stdio.h>
+#include <citro2d.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define SCREEN_WIDTH  40
-#define SCREEN_HEIGHT 30
-#define MAX_LENGTH    300
+#define CELL_SIZE 8
+#define GRID_WIDTH  40
+#define GRID_HEIGHT 30
+#define MAX_LENGTH  300
 
 typedef struct {
     int x, y;
 } Point;
 
+typedef enum { DIR_UP, DIR_DOWN, DIR_LEFT, DIR_RIGHT } Direction;
+
 int main() {
+    // Init systems
     gfxInitDefault();
-    consoleInit(GFX_TOP, NULL);
+    C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
+    C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
+    C2D_Prepare();
+    C3D_RenderTarget* top = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
 
-    srand(time(NULL));
+    // Colors
+    u32 colBG     = C2D_Color32(25, 20, 30, 255);
+    u32 colSnake  = C2D_Color32(80, 200, 120, 255);
+    u32 colHead   = C2D_Color32(50, 255, 80, 255);
+    u32 colFood   = C2D_Color32(255, 80, 80, 255);
+    u32 colText   = C2D_Color32(255, 255, 255, 255);
 
+    // Game state
     Point snake[MAX_LENGTH];
     int length = 3;
-    int dirX = 1, dirY = 0;
-    int foodX = rand() % SCREEN_WIDTH;
-    int foodY = rand() % SCREEN_HEIGHT;
-    int score = 0;
+    Direction dir = DIR_RIGHT;
     bool gameOver = false;
+    int foodX = rand() % GRID_WIDTH;
+    int foodY = rand() % GRID_HEIGHT;
+    int score = 0;
+    srand(time(NULL));
 
-    // Initialize snake in the middle
+    // Init snake in center
     for (int i = 0; i < length; i++) {
-        snake[i].x = SCREEN_WIDTH / 2 - i;
-        snake[i].y = SCREEN_HEIGHT / 2;
+        snake[i].x = GRID_WIDTH/2 - i;
+        snake[i].y = GRID_HEIGHT/2;
     }
 
+    // Main loop
     while (aptMainLoop()) {
         hidScanInput();
         u32 kDown = hidKeysDown();
 
         if (kDown & KEY_START) break;
         if (gameOver && (kDown & KEY_A)) {
-            // Restart
+            // Reset game
             length = 3;
-            dirX = 1; dirY = 0;
-            foodX = rand() % SCREEN_WIDTH;
-            foodY = rand() % SCREEN_HEIGHT;
+            dir = DIR_RIGHT;
+            foodX = rand() % GRID_WIDTH;
+            foodY = rand() % GRID_HEIGHT;
             score = 0;
             gameOver = false;
             for (int i = 0; i < length; i++) {
-                snake[i].x = SCREEN_WIDTH / 2 - i;
-                snake[i].y = SCREEN_HEIGHT / 2;
+                snake[i].x = GRID_WIDTH/2 - i;
+                snake[i].y = GRID_HEIGHT/2;
             }
         }
 
-        // Movement input
         if (!gameOver) {
-            if (kDown & KEY_DRIGHT && dirX != -1) { dirX = 1; dirY = 0; }
-            if (kDown & KEY_DLEFT && dirX != 1)  { dirX = -1; dirY = 0; }
-            if (kDown & KEY_DUP && dirY != 1)    { dirX = 0; dirY = -1; }
-            if (kDown & KEY_DDOWN && dirY != -1) { dirX = 0; dirY = 1; }
+            // Input
+            if ((kDown & KEY_DUP) && dir != DIR_DOWN) dir = DIR_UP;
+            else if ((kDown & KEY_DDOWN) && dir != DIR_UP) dir = DIR_DOWN;
+            else if ((kDown & KEY_DLEFT) && dir != DIR_RIGHT) dir = DIR_LEFT;
+            else if ((kDown & KEY_DRIGHT) && dir != DIR_LEFT) dir = DIR_RIGHT;
 
             // Move body
-            for (int i = length - 1; i > 0; i--) {
+            for (int i = length - 1; i > 0; i--)
                 snake[i] = snake[i - 1];
-            }
 
             // Move head
-            snake[0].x += dirX;
-            snake[0].y += dirY;
+            if (dir == DIR_UP) snake[0].y--;
+            if (dir == DIR_DOWN) snake[0].y++;
+            if (dir == DIR_LEFT) snake[0].x--;
+            if (dir == DIR_RIGHT) snake[0].x++;
 
-            // Collision with walls
-            if (snake[0].x < 0 || snake[0].x >= SCREEN_WIDTH ||
-                snake[0].y < 0 || snake[0].y >= SCREEN_HEIGHT)
+            // Check collisions
+            if (snake[0].x < 0 || snake[0].x >= GRID_WIDTH ||
+                snake[0].y < 0 || snake[0].y >= GRID_HEIGHT)
                 gameOver = true;
 
-            // Collision with self
-            for (int i = 1; i < length; i++) {
-                if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            for (int i = 1; i < length; i++)
+                if (snake[0].x == snake[i].x && snake[0].y == snake[i].y)
                     gameOver = true;
-                    break;
-                }
-            }
 
             // Eat food
             if (snake[0].x == foodX && snake[0].y == foodY) {
                 if (length < MAX_LENGTH) length++;
                 score++;
-                foodX = rand() % SCREEN_WIDTH;
-                foodY = rand() % SCREEN_HEIGHT;
+                foodX = rand() % GRID_WIDTH;
+                foodY = rand() % GRID_HEIGHT;
             }
         }
 
-        // Draw
-        consoleClear();
-        printf("=== SNAKE 3DS ===\n");
-        printf("Score: %d\n", score);
-        printf("Use D-Pad to move | START to quit\n");
+        // --- DRAW ---
+        C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+        C2D_TargetClear(top, colBG);
+        C2D_SceneBegin(top);
 
-        if (gameOver) {
-            printf("\nGAME OVER!\nPress A to restart.\n");
-        } else {
-            for (int y = 0; y < SCREEN_HEIGHT; y++) {
-                for (int x = 0; x < SCREEN_WIDTH; x++) {
-                    if (x == foodX && y == foodY) {
-                        printf("O");
-                    } else {
-                        bool printed = false;
-                        for (int i = 0; i < length; i++) {
-                            if (snake[i].x == x && snake[i].y == y) {
-                                printf(i == 0 ? "@" : "o");
-                                printed = true;
-                                break;
-                            }
-                        }
-                        if (!printed) printf(" ");
-                    }
-                }
-                printf("\n");
-            }
+        // Draw food
+        C2D_DrawRectSolid(foodX * CELL_SIZE, foodY * CELL_SIZE, 0,
+                          CELL_SIZE, CELL_SIZE, colFood);
+
+        // Draw snake
+        for (int i = 0; i < length; i++) {
+            u32 color = (i == 0) ? colHead : colSnake;
+            C2D_DrawRectSolid(snake[i].x * CELL_SIZE,
+                              snake[i].y * CELL_SIZE,
+                              0, CELL_SIZE, CELL_SIZE, color);
         }
 
-        gfxFlushBuffers();
-        gfxSwapBuffers();
-        gspWaitForVBlank();
+        // Text
+        C2D_TextBuf buf = C2D_TextBufNew(64);
+        C2D_Text text;
+        char msg[64];
+        if (!gameOver)
+            snprintf(msg, sizeof(msg), "Score: %d", score);
+        else
+            snprintf(msg, sizeof(msg), "Game Over! Score: %d\nPress A to restart", score);
+        C2D_TextParse(&text, buf, msg);
+        C2D_TextOptimize(&text);
+        C2D_DrawText(&text, C2D_WithColor, 2, 2, 0.5f, 0.6f, 0.6f, colText);
+        C2D_TextBufDelete(buf);
 
-        // Slow down
-        svcSleepThread(150000000); // ~0.15s per frame
+        C3D_FrameEnd(0);
+
+        svcSleepThread(100000000); // 0.1s per frame
     }
 
+    // Clean up
+    C2D_Fini();
+    C3D_Fini();
     gfxExit();
     return 0;
 }
